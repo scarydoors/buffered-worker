aaf8ea0e9b47ed3bde740da697df9e5de6bf6990
Alan Strauhs <scarydoorsyeah@gmail.com>
2025-07-14 21:48:21 +0100

    wip: bad implementation of graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"runtime"
 	"time"
 )
 
@@ -21,37 +23,47 @@ func NewBufferedWorker[T any](maxItems int, flushInterval time.Duration, callbac
 	}
 }
 
-func (b *BufferedWorker[T]) Start() chan<- T {
+func (b *BufferedWorker[T]) Start(ctx context.Context) (chan<- T, <-chan struct{}){
 	itemCh := make(chan T)
-	ticker := time.NewTicker(b.flushInterval)
+	done := make(chan struct{})
+	tickerCh := time.Tick(b.flushInterval)
 
 	go func() {
+		defer close(itemCh)
+		defer close(done)
+		defer fmt.Println("worker stopped")
 		for {
 			select {
-			case <-ticker.C:
+			case <-tickerCh:
 				b.flush()
 			case item := <-itemCh:
 				b.items = append(b.items, item)
 				if len(b.items) >= b.maxItems {
 					b.flush()
 				}
+			case <-ctx.Done():
+				// TODO: flush the rest
+				return
 			}
 		}
 	}()
 
-	return itemCh
+	return itemCh, done
 }
 
 func (b *BufferedWorker[T]) flush() {
 	b.callback(b.items)
 	b.items = b.items[:0]
 }
+
 func main() {
+	fmt.Println("Number of Goroutines:", runtime.NumGoroutine())
 	worker := NewBufferedWorker(4, time.Second*5, func(t []int) {
 		fmt.Printf("Flushed %v\n", t)
 	})
 
-	itemCh := worker.Start()
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	itemCh, done := worker.Start(ctx)
 
 	itemCh <- 5
 	itemCh <- 5
@@ -59,5 +71,7 @@ func main() {
 	itemCh <- 5
 	itemCh <- 5
 
-	time.Sleep(7 * time.Second)
+	time.Sleep(4 * time.Second)
+	cancelCtx()
+	<-done
 }
